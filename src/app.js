d09f689370e590c372360f5c7fc57c6d1f38e920
Ajeet Singh Bhati <bhatiajeet017@gmail.com>
2025-01-16 00:16:25 +0530

    play with routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express = require("express")
 
 const app = express()
 
+app.get("/user/:userId/:userName",(req, res) => {
+    //console.log("Request query",req.query)
+    console.log("Request params",req.params)
+    res.send({"name": "Ajeet", "area": "jodhpur"})
+})
+
+app.post("/user", (req, res)=> {
+    //data will come in req and same to db
+    res.send("Data saved successfully")
+})
+
+app.delete("/user", (req, res)=> {
+    res.send("Data deleted successfully")
+})
+
 app.use("/test",(req, res) => {
     res.send("Namaste from the test path")
-})
-
-app.use("/hello",(req, res) => {
-    res.send("Namaste from the hello path")
-})
-
-app.use("/",(req, res) => {
-    res.send("Namaste from the default path")
 })
 
 app.listen(7777, () => {
